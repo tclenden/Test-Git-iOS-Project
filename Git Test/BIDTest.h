d4bdb1c7b4607172bc2da77f410340d711a48f5a
Thomas Clendennen <thomasclendennen@thomass-mbp.hsd1.tn.comcast.net>
2014-09-04 22:34:28 -0500

    added a test class

---
+++ Git Test/BIDTest.h
@@ -0,0 +1,13 @@
+//
+//  BIDTest.h
+//  Git Test
+//
+//  Created by Thomas Clendennen on 9/4/14.
+//  Copyright (c) 2014 Thomas Clendennen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BIDTest : NSObject
+
+@end
---
+++ Git Test/BIDTest.m
@@ -0,0 +1,13 @@
+//
+//  BIDTest.m
+//  Git Test
+//
+//  Created by Thomas Clendennen on 9/4/14.
+//  Copyright (c) 2014 Thomas Clendennen. All rights reserved.
+//
+
+#import "BIDTest.h"
+
+@implementation BIDTest
+
+@end
